f27b8b928b486d03126ed16be2b0e775c21aa30b
Kevin Juister <contact@n-x.xyz>
2021-02-17 19:58:41 +0100

    Init

---
+++ ValheimPlus/Initialize.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using Unity;
+using UnityEngine;
+using System.IO;
+using System.Reflection;
+using System.Runtime;
+using IniParser;
+using IniParser.Model;
+using HarmonyLib;
+using System.Globalization;
+using Steamworks;
+
+
+namespace ValheimServerSettings
+{
+    // COPYRIGHT 2021 KEVIN "nx#8830" J. // http://n-x.xyz
+    // SOURCECODE DERIVATE OF VALHEIM PLUS
+    // https://github.com/nxPublic/ValheimPlus
+
+    [BepInPlugin("org.bepinex.plugins.valheim_plus", "Valheim Server Settings", "0.1")]
+    class ValheimServerSettingsPlugin : BaseUnityPlugin
+    {
+        
+        public static string version = "0.7";
+        public static string newestVersion = "";
+        public static Boolean isUpToDate = false;
+
+        string ConfigPath = Path.GetDirectoryName(Paths.BepInExConfigPath) + "\\valheim_server_settings.cfg";
+
+        // DO NOT REMOVE MY CREDITS
+        public static string Author = "Kevin 'nx' J.";
+        public static string Website = "http://n-x.xyz";
+        public static string Discord = "nx#8830";
+        public static string Repository = "https://github.com/nxPublic/ValheimServerSettings";
+
+        // Add your credits here in case you modify the code or make additions, feel free to add as many as you like
+        String ModifiedBy = "YourName";
+
+        public static Boolean isDebug = false;
+
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+
+
+
+            Logger.LogInfo("Trying to load the configuration file");
+            if (File.Exists(ConfigPath))
+            {
+                Logger.LogInfo("Configuration file found, loading configuration.");
+                if (ValheimServerSettings.Settings.LoadSettings() != true)
+                {
+                    Logger.LogError("Error while loading configuration file.");
+                }
+                else
+                {
+
+                    Logger.LogInfo("Configuration file loaded succesfully.");
+
+                    var harmony = new Harmony("mod.valheim_server_settings");
+                    harmony.PatchAll();
+                }
+            }
+            else
+            {
+                Logger.LogError("Error: File not found. Plugin not loaded.");
+            }
+        }
+
+
+        
+    }
+}
---
+++ ValheimPlus/Settings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using Unity;
+using UnityEngine;
+using System.IO;
+using System.Reflection;
+using System.Runtime;
+using IniParser;
+using IniParser.Model;
+using HarmonyLib;
+using System.Globalization;
+using Steamworks;
+using System.Net;
+
+// Todo, better error handling
+
+namespace ValheimServerSettings
+{
+    class Settings
+    {
+
+        public static IniData Config { get; set; }
+        public static IniData Defaults { get; set; }
+
+        static string ConfigPath = Path.GetDirectoryName(Paths.BepInExConfigPath) + "\\valheim_plus.cfg";
+        static string ConfigDefaultsPath = Path.GetDirectoryName(Paths.BepInExConfigPath) + "\\valheim_plus_defaults.cfg";
+
+        public static bool LoadSettings()
+        {
+            try
+            {
+                IniData userConfig;
+                var parser = new FileIniDataParser();
+
+                userConfig = parser.ReadFile(ConfigPath);
+                Defaults = parser.ReadFile(ConfigDefaultsPath);
+
+                Defaults.Merge(userConfig);
+
+                Config = Defaults;
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool isEnabled(string section)
+        {
+            return Boolean.Parse(Config[section]["enabled"]);
+        }
+        public static bool getBool(string section, string name)
+        {
+            return (Config[section][name].ToLower() == "true");
+        }
+        public static string getString(string section, string name)
+        {
+            return Config[section][name];
+        }
+        public static int getInt(string section, string name)
+        {
+            return int.Parse(Config[section][name]);
+        }
+        public static float getFloat(string section, string name)
+        {
+            
+            return float.Parse(Config[section][name], CultureInfo.InvariantCulture.NumberFormat);
+        }
+        public static KeyCode getHotkey(string name)
+        {
+            KeyCode HotKey = KeyCode.None;
+            try
+            {
+                HotKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), Config["Hotkeys"][name]);
+            }
+            catch(Exception e){
+                HotKey = KeyCode.None;
+            }
+            
+            return HotKey;
+        }
+
+    }
+}
